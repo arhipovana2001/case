b3117864e1b7655b12845d86b45cee8b4e2c3e7e
AlenaNiki <ms.alenkanikki@mail.ru>
2019-09-21 12:50:45 +0500

    Completed tasks.

--- main.py
+++ main.py
@@ -74,9 +74,29 @@ def triangle_7(x, y, a, b):
 
 def parallelogram_1(x, y, a, b):
     # TODO (Nikitina.A) Draw parallelogram
+    turtle.up()
+    turtle.setposition(x, y)
+    turtle.down()
+    turtle.forward(a)
+    turtle.right(135)
+    turtle.forward(b)
+    turtle.right(45)
+    turtle.forward(a)
+    turtle.right(135)
+    turtle.forward(b)
+    turtle.right(45)
 
 def triangle_8(x, y, a, b):
     # TODO (Nikitina.A) Draw triangle
+    turtle.up()
+    turtle.setposition(x, y)
+    turtle.down()
+    turtle.forward(a)
+    turtle.right(90)
+    turtle.forward(a)
+    turtle.right(135)
+    turtle.forward(b)
+    turtle.right(90)
 
 def main():
         turtle.fillcolor("red")
@@ -100,5 +120,15 @@ def main():
         triangle_5(50, 50, 25 * math.sqrt(2), 50)
         turtle.end_fill()
         turtle.done()
-
+    # ALENA NIKITINA DEF MAIN
+    #turtle.fillcolor('green')
+    #turtle.begin_fill()
+    #parallelogram_2(25, 75, 50, 25 * math.sqrt(2))
+    #turtle.end_fill()
+    #turtle.left(45)
+    #turtle.fillcolor("pink")
+    #turtle.begin_fill()
+    #triangle_2(25, 75, 25 * math.sqrt(2), 50)
+    #turtle.end_fill()
+    
 main()
