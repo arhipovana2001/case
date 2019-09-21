e10d8d557d18e92edf42e87123ecfdaa06287556
arhipovana2001 <arhipovana2001@mail.ru>
2019-09-21 15:19:19 +0700

    Полный код

--- main.py
+++ main.py
@@ -6,18 +6,6 @@
 import turtle
 import math
 
-def triangle_1(x, y, a): #blue
-    turtle.up()
-    turtle.setposition(x, y)
-    turtle.down()
-    turtle.right(90)
-    turtle.forward(a)
-    turtle.right(90)
-    turtle.forward(a)
-    turtle.right(135)
-    turtle.forward(a*(math.sqrt(2)))
-
-
 def triangle_3(x, y, a, b): #redNastya
         turtle.up()
         turtle.setposition(x, y)
@@ -28,7 +16,7 @@ def triangle_3(x, y, a, b): #redNastya
         turtle.right(90)
         turtle.forward(b)
 
-def triangle_4(x, y, a, b): #yellow
+def triangle_4(x, y, a, b): #yellowNastya
         turtle.up()
         turtle.setposition(x, y)
         turtle.down()
@@ -39,29 +27,7 @@ def triangle_4(x, y, a, b): #yellow
         turtle.right(135)
         turtle.forward(b)
 
-def square_1(x,y,a): #orange
-    turtle.up()
-    turtle.setposition(x, y)
-    turtle.down()
-    turtle.right(360)
-    turtle.forward(a)
-    turtle.right(90)
-    turtle.forward(a)
-    turtle.right(90)
-    turtle.forward(a)
-    turtle.right(90)
-    turtle.forward(a)
 
-def triangle_5(x,y,a,b): #purple
-    turtle.up()
-    turtle.setposition(x,y)
-    turtle.down()
-    turtle.right(90)
-    turtle.forward(2*a)
-    turtle.right(135)
-    turtle.forward(b)
-    turtle.right(135)
-    turtle.forward(a)
 
 def triangle_6(x, y, a):
     # TODO (Revtova.L) Draw triangle
@@ -136,20 +102,8 @@ def main():
         turtle.begin_fill()
         triangle_4(0,0, 50 * math.sqrt(2), 100)
         turtle.end_fill()
-        turtle.fillcolor("blue")
-        turtle.begin_fill()
-        triangle_1(100, 50, 50)
-        turtle.end_fill()
-        turtle.fillcolor("orange")
-        turtle.begin_fill()
-        square_1(50, 50, 25 * math.sqrt(2))
-        turtle.end_fill()
-        turtle.fillcolor("purple")
-        turtle.begin_fill()
-        triangle_5(50, 50, 25 * math.sqrt(2), 50)
-        turtle.end_fill()
         turtle.done()
-    # ALENA NIKITINA DEF MAIN
+    #ALENA NIKITINA DEF MAIN
     #turtle.fillcolor('green')
     #turtle.begin_fill()
     #parallelogram_2(25, 75, 50, 25 * math.sqrt(2))
