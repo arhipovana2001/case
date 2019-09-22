725dd0136b8282ef577292944767ce14e16f7c8b
LidaRevtova <lida.revtova@yandex.ru>
2019-09-22 22:14:01 +0700

    верблюд

--- main.py
+++ main.py
@@ -18,31 +18,6 @@ def triangle_3(x, y, a, b): #redNastya
     right(90)
     forward(b)
 
-def triangle_4(x, y, a, b): #yellowNastya
-        turtle.up()
-        turtle.setposition(x, y)
-        turtle.down()
-        turtle.right(180)
-        turtle.forward(a)
-        turtle.right(90)
-        turtle.forward(a)
-        turtle.right(135)
-        turtle.forward(b)
-
-
-
-def triangle_6(x, y, a):
-    # TODO (Revtova.L) Draw triangle
-    up()
-    setposition(x, y)
-    down()
-    right(90)
-    forward(a)
-    right(90)
-    forward(a)
-    right(135)
-    forward(a * (math.sqrt(2)))
-
 def square_2(x, y, a):
     # TODO (Revtova.L) Draw square
     up()
@@ -57,18 +32,6 @@ def square_2(x, y, a):
     right(90)
     fd(a)
 
-def triangle_7(x, y, a, b):
-    # TODO (Revtova.L) Draw triangle
-    up()
-    setposition(x, y)
-    down()
-    right(90)
-    forward(2 * a)
-    right(135)
-    forward(b)
-    right(135)
-    forward(a)
-
 def parallelogram_1(x, y, a, b):
     # TODO (Nikitina.A) Draw parallelogram
     up()
@@ -81,18 +44,6 @@ def parallelogram_1(x, y, a, b):
     fd(a)
     right(45)
     fd(b)
-
-def triangle_8(x, y, a, b):
-    # TODO (Nikitina.A) Draw triangle
-    turtle.up()
-    turtle.setposition(x, y)
-    turtle.down()
-    turtle.forward(a)
-    turtle.right(90)
-    turtle.forward(a)
-    turtle.right(135)
-    turtle.forward(b)
-    turtle.right(90)
 
 def rabbit():
     fillcolor("green")
@@ -163,9 +114,46 @@ def fish():
     right(270)
     triangle_3(-247-50* math.sqrt(2),-25* math.sqrt(2),50,25 *math.sqrt(2))
     end_fill()
-    done()
-
 fish()
+
+def camel():
+    fillcolor("green")
+    begin_fill()
+    right(45)
+    parallelogram_1(200,0, 25 * math.sqrt(2), 50)
+    end_fill()
+    fillcolor("red")
+    begin_fill()
+    right(90)
+    triangle_3(200+34*math.sqrt(2),4, 100, 50*math.sqrt(2))
+    end_fill()
+    fillcolor("blue")
+    begin_fill()
+    right(45)
+    triangle_3(200+34*math.sqrt(2),54*math.sqrt(2),50*math.sqrt(2),50)
+    end_fill()
+    fillcolor("yellow")
+    begin_fill()
+    right(45)
+    triangle_3(200 + 84 * math.sqrt(2), 54/3 * math.sqrt(2),100,50 * math.sqrt(2))
+    end_fill()
+    fillcolor("orange")
+    begin_fill()
+    right(45)
+    square_2(200 + 84 * math.sqrt(2), 54/3 * math.sqrt(2), 25 * math.sqrt(2))
+    end_fill()
+    fillcolor("pink")
+    begin_fill()
+    right(45)
+    triangle_3(202+50/4+84 * math.sqrt(2),54/3 * math.sqrt(2)+25*math.sqrt(2) , 50, 25* math.sqrt(2))
+    end_fill()
+    fillcolor("purple")
+    begin_fill()
+    right(180)
+    triangle_3(200 + 71.5* math.sqrt(2), 5, -50, -25* math.sqrt(2))
+    end_fill()
+    done()
+camel()
 
 
 
